b2f7b70aabbe445c4c7df92dbd12229a8b1184dd
DianaG <diana.cg93@gmail.com>
2020-11-03 22:27:52 -0500

    class lab

--- target-sum-exists/target-sum-exists.js
+++ target-sum-exists/target-sum-exists.js
@@ -1,6 +1,12 @@
 const targetSumExistsInTwoNumbers = (list, target) => {
-  return false
-}
+  for (let i in list) {
+    for (let j in list) {
+      if (list[i] + list[j] === target) {
+        return true;
+      }
+    }
+  }
+};
 
 const targetSumExistsInThreeNumbers = (list, target) => {
   return false
