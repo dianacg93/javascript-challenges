0220236d0cb825d5f25d16cfa556c64e3844722d
DianaG <diana.cg93@gmail.com>
2020-11-13 00:03:40 -0500

    stack

--- stack-machine-calculator/stack-machine-calculator.js
+++ stack-machine-calculator/stack-machine-calculator.js
@@ -1,5 +1,25 @@
 const stackMachineCalculator = (instructions) => {
-  return instructions
+  let input = instructions.split(" ")
+  let stack = []
+
+  for(let i = 0 ; i < input.length; i++){
+    el = input[i];
+    if(el == 'POP'){
+      stack.pop();
+    } 
+    else if(el == 'DUP'){
+      stack.splice(stack.length, 0, stack[stack.length-1]);
+    }
+    else if(el == '+'){
+      stack.push(stack.pop() + stack.pop());
+    }
+    else if(el == '-'){
+      stack.push(stack.pop()-stack.pop());
+    } else {
+      stack.push(el);
+    }
+  }
+  return Number(stack[stack.length-1]);
 }
 
 module.exports = stackMachineCalculator
