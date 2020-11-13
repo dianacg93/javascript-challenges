5d7edda8d710d497657cc289af3f72d1e473dc33
DianaG <diana.cg93@gmail.com>
2020-11-12 21:58:26 -0500

    balanced parens

--- balanced-parens/balanced-parens.js
+++ balanced-parens/balanced-parens.js
@@ -1,5 +1,35 @@
 const parensAreBalanced = (input) => {
-  return false
+  // console.log(input)
+  if(input.length == 1){return false}
+  
+  const stack = [];
+  for(let i = 0; i < input.length; i++){
+    let el = input[i];
+    if(el == '[' || el == '{' || el == '('){
+      stack.push(el);
+    }
+    if(el == ']'){
+      let last = stack.pop();
+      if(last != '['){
+        return false
+      }
+    } else if(el == '}'){
+      let last = stack.pop();
+      if(last != '{'){
+        return false
+      }
+    } else if(el == ')'){
+      let last = stack.pop();
+      if(last != '('){
+        return false
+      }
+    }
+  }
+  if(stack.length){
+    return false;
+  }
+  
+  return true;
 }
 
 module.exports = parensAreBalanced
