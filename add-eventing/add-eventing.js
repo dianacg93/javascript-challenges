72a01a44d7926618f932b457fae4b8857bccee0b
DianaG <diana.cg93@gmail.com>
2020-11-12 23:47:02 -0500

    add eventing

--- add-eventing/add-eventing.js
+++ add-eventing/add-eventing.js
@@ -1,4 +1,19 @@
 const addEventing = function (obj) {
+    let events = {}
+
+  obj.on = (event, func) => {
+    if (events[event]) {
+      events[event].push(func)
+    } else {
+      events[event] = [func]
+    }
+  }
+
+  obj.trigger = (event, ...args) => {
+    events[event].forEach(func => func(...args))
+  }
+
+  return obj
 }
 
 module.exports = addEventing
